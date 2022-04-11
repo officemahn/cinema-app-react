6caa9279498065be56f354b9a087e4f514ce532b
David Oladipo <David.Oladipo@outlook.com>
2022-04-11 13:38:58 -0600

    added slideshow component to display images automatically

---
+++ src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainContent from '../content/main-content/MainContent';
+
+const main = () => {
+  return (
+    <div className="main">
+      <MainContent />
+    </div>
+  );
+};
+
+export default main;
